3d5abb94386eb4f0f044fc23d839ff54d5967dbd
Arun Ramachandran <sar_584@yahoo.co.in>
2020-07-28 11:26:02 +0530

    Adding to repository

---
+++ src/main/java/com/sample/project/SampleJavaApp.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.sample.project;
+
+/**
+ * @author arunramachand.subra
+ *
+ */
+public class SampleJavaApp {
+
+	public static void main(String[] args) {
+		System.out.println("1. This is the master branch code");
+	}
+}
