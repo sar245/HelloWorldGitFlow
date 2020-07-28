7a934dda0524de194436da9b4246dfb5c6d67e96
Arun Ramachandran <sar_584@yahoo.co.in>
2020-07-28 13:53:47 +0530

    Merged to develop

--- src/main/java/com/sample/project/SampleJavaApp.java
+++ src/main/java/com/sample/project/SampleJavaApp.java
@@ -11,9 +11,13 @@ public class SampleJavaApp {
 
 	public static void main(String[] args) {
 		System.out.println("1. This is the master branch code");
-<<<<<<< HEAD
+		feature1();
 		feature2();
 		feature2BugFix1();
+	}
+	
+	public static void feature1() {
+		System.out.println("2. This is a feature implementation");
 	}
 	
 	public static void feature2() {
@@ -22,12 +26,7 @@ public class SampleJavaApp {
 	
 	public static void feature2BugFix1() {
 		System.out.println("3. This is the 2nd feature Bug Fix 1");
-=======
-		feature1();
 	}
 	
-	public static void feature1() {
-		System.out.println("2. This is a feature implementation");
->>>>>>> feature_branch
-	}
+
 }
